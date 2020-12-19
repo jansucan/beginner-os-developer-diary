d97b3ed5eece5bf2efad66f7c9a898322dee4cb5
Jan Sucan <jan@jansucan.com>
2020-12-19 10:07:19 +0100

    src: Add missing include to a header file

--- src/kernel/init/terminal.h
+++ src/kernel/init/terminal.h
@@ -1,6 +1,8 @@
 #ifndef TERMINAL_H
 #define TERMINAL_H
 
+#include <stdint.h>
+
 void terminal_initialize(void);
 void terminal_write_string(const char *const string);
 void terminal_write_uint32(uint32_t number);
